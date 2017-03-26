f8edfbcc4d01995febab5f2f1fc8822ed4a80d70
Seong-Kyun <manggoguy@naver.com>
2017-03-26 17:22:48 +0900

    Add new settings

---
+++ src/main/java/com/riotgames/sample/MyOwnRuntimeException.java
@@ -0,0 +1,7 @@
+package com.riotgames.sample;
+//Test
+public class MyOwnRuntimeException extends RuntimeException{
+    public MyOwnRuntimeException(Throwable cause) {
+        super(cause);
+    }
+}
